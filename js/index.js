cf928ffb1a1638145db8674e77bdc937cb79dc4f
leerick573 <1067346925@qq.com>
2020-08-29 00:14:55 +0800

    boot项目最终提交

---
+++ js/index.js
@@ -0,0 +1,47 @@
+
+			$(function(){
+				var index = 0;
+				var fun;
+				dingshiqi();
+				function dingshiqi() {
+					fun = setInterval(function(){
+					if(index==$(".box-img").length-1){
+						index = 0;
+						$(".box-img").css("opacity","0");
+						$(".box-img").eq(index).css("opacity","1");
+					}else{
+						index++;
+						$(".box-img").css("opacity","0");
+						$(".box-img").eq(index).css("opacity","1");
+					}
+				},3000)
+				}
+				$(".box-left").click(function(){
+					clearInterval(fun);
+					if(index==0){
+						index = $(".box-img").length-1;
+						$(".box-img").css("opacity","0");
+						$(".box-img").eq(index).css("opacity","1");
+						dingshiqi();
+					}else{
+						index--;
+						$(".box-img").css("opacity","0");
+						$(".box-img").eq(index).css("opacity","1");
+						dingshiqi();
+					}
+				})
+				$(".box-right").click(function(){
+					clearInterval(fun);
+					if(index==$(".box-img").length-1){
+						index = 0;
+						$(".box-img").css("opacity","0");
+						$(".box-img").eq(index).css("opacity","1");
+						dingshiqi();
+					}else{
+						index++;
+						$(".box-img").css("opacity","0");
+						$(".box-img").eq(index).css("opacity","1");
+						dingshiqi();
+					}
+				})
+			})
